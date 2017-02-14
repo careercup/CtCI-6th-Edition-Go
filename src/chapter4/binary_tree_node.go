f92c0121f574c114a7953442b7886f7694ddb473
motomux <motomux@gmail.com>
2017-02-14 00:34:47 -0800

    Add problem 6 on chapter 4

--- src/chapter4/binary_tree_node.go
+++ src/chapter4/binary_tree_node.go
@@ -2,7 +2,20 @@ package chapter4
 
 // BTNode represents a node in binary tree
 type BTNode struct {
-	Value int
-	Left  *BTNode
-	Right *BTNode
+	Value  int
+	Parent *BTNode
+	Left   *BTNode
+	Right  *BTNode
+}
+
+// SetRight sets child on right side
+func (n *BTNode) SetRight(c *BTNode) {
+	n.Right = c
+	c.Parent = n
+}
+
+// SetLeft sets child on right side
+func (n *BTNode) SetLeft(c *BTNode) {
+	n.Left = c
+	c.Parent = n
 }
