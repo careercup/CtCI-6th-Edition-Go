9f62ac97b7a46dfe4ffd3ea65a4f24b895b40872
Vikas Goel <vikas.goel@gmail.com>
2018-07-04 22:51:48 -0700

    Problem 8.11 Coins

--- src/chapter8/coin-change.go
+++ src/chapter8/coin-change.go
@@ -2,34 +2,37 @@ package main
   
 import "fmt"
 
-func count(coin []int, sum int) int {
-	numCoins := len(coin)
-	C := make([][]int, sum+1)
+func count(denoms []int, cents int) int {
+	numDenoms := len(denoms)
+	ways := make([][]int, cents+1)
 
-	for i := 0; i <= sum; i++ {
-		C[i] = make([]int, numCoins)
+	for i := 0; i <= cents; i++ {
+		ways[i] = make([]int, numDenoms)
 	}
 
-	for i := 0; i < numCoins; i++ {
-		C[0][i] = 1
+	// Zero cent can be done in 1 way only irrespective of denominations.
+	for i := 0; i < numDenoms; i++ {
+		ways[0][i] = 1
 	}
 
-	for i := 1; i <= sum; i++ {
-		for j := 0; j < numCoins; j++ {
-			if i - coin[j] >= 0 {
-				C[i][j] += C[i-coin[j]][j]
+	for i := 1; i <= cents; i++ {
+		for j := 0; j < numDenoms; j++ {
+			// Include denom[j] to count the ways.
+			if i - denoms[j] >= 0 {
+				ways[i][j] += ways[i-denoms[j]][j]
 			}
 
+			// Exclude denom[j] to count the ways.
 			if j >= 1 {
-				C[i][j] += C[i][j-1]
+				ways[i][j] += ways[i][j-1]
 			}
 		}
 	}
 
-	return C[sum][numCoins-1]
+	return ways[cents][numDenoms-1]
 }
 
 func main() {
-	coin := []int{1, 5, 10, 25}
-	fmt.Println(count(coin, 100))
+	denoms := []int{1, 5, 10, 25}
+	fmt.Println(count(denoms, 100))
 }
