b4eb8e7f40caf3648af026261c7bf83f005f737d
Shang, Rocky <tshang@ebay.com>
2019-02-25 20:07:31 +0800

    add problem 2 - 2nd solution of charter 1

--- src/chapter1/problem2.go
+++ src/chapter1/problem2.go
@@ -18,3 +18,21 @@ func ArePermutations(input1, input2 string) bool {
 	}
 	return true
 }
+
+func ArePermutationsB(input1, input2 string) bool {
+	if len(input1) != len(input2) {
+		return false
+	}
+	counts := make(map[rune]int)
+	for _, r := range input1 {
+		counts[r]++
+	}
+	for _, r := range input2 {
+		counts[r]--
+		if counts[r] < 0 {
+			return false
+		}
+	}
+
+	return true
+}
--- src/chapter1/problem2_test.go
+++ src/chapter1/problem2_test.go
@@ -25,3 +25,26 @@ func TestArePermutations(t *testing.T) {
 		}
 	}
 }
+
+func TestArePermutationsB(t *testing.T) {
+	cases := []struct {
+		input1   string
+		input2   string
+		expected bool
+	}{
+		{"abcd", "abcd", true},
+		{"abcd", "abdc", true},
+		{"abcc", "ccbb", false},
+		{"abcc", "abcc ", false},
+		{"abccc", "abccb", false},
+		{" ", " ", true},
+		{"", "", true},
+	}
+	for _, c := range cases {
+		actual := ArePermutationsB(c.input1, c.input2)
+		if actual != c.expected {
+			t.Fatalf("Inputs %s, %s. Expected: %t, actual: %t\n",
+				c.input1, c.input2, c.expected, actual)
+		}
+	}
+}
