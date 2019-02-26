29182e56f027dbdcd2a3b08811bf2c1663da0f10
Shang, Rocky <tshang@ebay.com>
2019-02-26 22:29:41 +0800

    add problem 4 - solution B of charter 1

--- src/chapter1/problem4_test.go
+++ src/chapter1/problem4_test.go
@@ -22,3 +22,24 @@ func TestIsPalindromePerm(t *testing.T) {
 		}
 	}
 }
+
+func TestIsPalindromePermB(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected bool
+	}{
+		{"My rats live on no evil star", false}, // not a  palindrome
+		{"Rats live on no evil star", true},     // normal palindrome
+		{"amanaplanacanalpanama", true},         // normal palindrome
+		{"amanalpancaaanplanama", true},         // jumbled palindrome
+		{"amanaplanacanalpanamab", false},       // not a palindrome
+		{"a", true},
+		{"", true},
+	}
+	for _, c := range cases {
+		actual := IsPalindromePermB(c.input)
+		if actual != c.expected {
+			t.Fatalf("Input %s. Expected: %t, actual: %t\n", c.input, c.expected, actual)
+		}
+	}
+}
