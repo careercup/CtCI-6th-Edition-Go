0bee34671f62e7f9b94ff1ddc86ae90f0c3cdb84
Shang, Rocky <tshang@ebay.com>
2019-07-17 22:07:44 +0800

    add o(1) space for 1.8

--- src/chapter1/problem8_test.go
+++ src/chapter1/problem8_test.go
@@ -66,3 +66,65 @@ func TestZeroMatrix(t *testing.T) {
 		}
 	}
 }
+
+func TestZeroMatrixB(t *testing.T) {
+	cases := []struct {
+		input    [][]int
+		expected [][]int
+	}{
+		{
+			[][]int{
+				[]int{1, 2, 3},
+				[]int{4, 0, 6},
+				[]int{7, 8, 9},
+			},
+			[][]int{
+				[]int{1, 0, 3},
+				[]int{0, 0, 0},
+				[]int{7, 0, 9},
+			},
+		},
+		{
+			[][]int{
+				[]int{1, 2, 3},
+				[]int{4, 5, 6},
+				[]int{7, 8, 0},
+			},
+			[][]int{
+				[]int{1, 2, 0},
+				[]int{4, 5, 0},
+				[]int{0, 0, 0},
+			},
+		},
+		{
+			[][]int{
+				[]int{1, 2, 3},
+				[]int{0, 5, 6},
+				[]int{7, 8, 9},
+			},
+			[][]int{
+				[]int{0, 2, 3},
+				[]int{0, 0, 0},
+				[]int{0, 8, 9},
+			},
+		},
+		{
+			[][]int{
+				[]int{1, 2, 3},
+				[]int{4, 5, 6},
+				[]int{7, 8, 9},
+			},
+			[][]int{
+				[]int{1, 2, 3},
+				[]int{4, 5, 6},
+				[]int{7, 8, 9},
+			},
+		},
+	}
+	for _, c := range cases {
+		ZeroMatrixB(c.input)
+		if !reflect.DeepEqual(c.input, c.expected) {
+			t.Fatalf("Expected: %v, actual: %v\n", c.expected, c.input)
+		}
+	}
+}
