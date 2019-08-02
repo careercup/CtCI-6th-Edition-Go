4be92a607496ea9c023cf8bb1882981f6f43df4c
Shang, Rocky <tshang@ebay.com>
2019-08-02 21:35:49 +0800

    add case for 2.4

--- src/chapter2/problem4.go
+++ src/chapter2/problem4.go
@@ -19,3 +19,33 @@ func (ll *DoublyLinkedList) PivotAroundValue(value int) {
 	ll.tail = higher.tail
 	ll.head = lower.head
 }
+
+func (ll *DoublyLinkedList) PivotAroundValueB(value int) {
+	lower := GetLinkedList()
+	higher := GetLinkedList()
+	middle := GetLinkedList()
+	node := ll.head
+	for i := 0; i < ll.count; i++ {
+		next := node.next
+		if node.value == value {
+			middle.insertNode(node)
+		} else if node.value < value {
+			lower.insertNode(node)
+		} else {
+			higher.insertNode(node)
+		}
+		node = next
+	}
+	// Join higher and lower lists, and replace ll.
+	if middle.Len() > 0 {
+		lower.tail.next = middle.head
+		middle.head.prev = lower.tail
+		middle.tail.next = higher.head
+		higher.head.prev = middle.tail
+	} else {
+		lower.tail.next = higher.head
+		higher.head.prev = lower.tail
+	}
+	ll.tail = higher.tail
+	ll.head = lower.head
+}
